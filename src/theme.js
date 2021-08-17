dfaeceb0627e37177e140e147c1ffa25347d8ffa
VirajKanse <virajv2k@gmail.com>
2021-08-17 10:31:34 +0530

    cleanup & setup

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+    fonts: {
+        body: 'Red Hat Display',
+        heading: 'Red Hat Display'
+    }
+})
+ 
+export default theme;
